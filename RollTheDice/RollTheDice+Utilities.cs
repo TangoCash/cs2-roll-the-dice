84ebe5f44f03ae7521e1272469c6d722df6e8b5b
Kalle <kalle@kandru.de>
2024-11-03 16:32:37 +0100

    add unused function to get playermodel (not properly working yet)

--- RollTheDice/RollTheDice+Utilities.cs
+++ RollTheDice/RollTheDice+Utilities.cs
@@ -1,5 +1,7 @@
+using System.Runtime.InteropServices;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
 
 namespace RollTheDice
 {
@@ -22,6 +24,15 @@ namespace RollTheDice
                 if (alert) AddTimer(delay, () => player.PrintToCenterAlert(message));
                 else AddTimer(delay, () => player.PrintToCenterHtml(message));
             }
+        }
+
+        private static string GetPlayerModel(CCSPlayerPawn playerPawn)
+        {
+            var signature = "\\x40\\x53\\x48\\x83\\xEC\\x20\\x48\\x8B\\x41\\x30\\x48\\x8B\\xD9\\x48\\x8B\\x48\\x08\\x48\\x8B\\x01\\x2A\\x2A\\x2A\\x48\\x85";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) signature = "\\x55\\x48\\x89\\xE5\\x53\\x48\\x89\\xFB\\x48\\x83\\xEC\\x08\\x48\\x8B\\x47\\x38";
+            var getModel = new VirtualFunctionWithReturn<IntPtr, string>(signature);
+            string model = getModel.Invoke(playerPawn.Handle);
+            return model;
         }
     }
 }
