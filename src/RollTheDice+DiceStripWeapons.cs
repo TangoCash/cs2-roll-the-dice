62132a0d89a3c4e18a911fed67cb3672b2fcf0b9
Kalle <kalle@kandru.de>
2024-11-11 22:22:16 +0100

    fix: strip player weapons correctly

--- src/RollTheDice+DiceStripWeapons.cs
+++ src/RollTheDice+DiceStripWeapons.cs
@@ -9,34 +9,22 @@ namespace RollTheDice
         {
             if (playerPawn.WeaponServices == null) return Localizer["command.rollthedice.error"].Value.Replace("{playerName}", player.PlayerName);
             var playerWeapons = playerPawn.WeaponServices!;
-            uint weaponRaw = 0;
             foreach (var weapon in playerWeapons.MyWeapons)
             {
                 // ignore unknown weapons
                 if (weapon.Value == null || weapon.Value != null && weapon.Value.DesignerName == null) continue;
-                // ignore knife and C4
+                Console.WriteLine(weapon.Value!.DesignerName);
                 if (weapon.Value!.DesignerName == $"weapon_{CsItem.C4.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.Bomb.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == "weapon_knife" // necessary because CsItem.Knife is not always this value
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.Knife.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.KnifeCT.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.KnifeT.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.DefaultKnifeCT.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.DefaultKnifeT.ToString().ToLower()}")
+                    || weapon.Value!.DesignerName == $"weapon_{CsItem.Bomb.ToString().ToLower()}")
                 {
-                    // save weapon raw
-                    weaponRaw = weapon.Raw;
-                    continue;
+                    // change weapon to currently active weapon
+                    playerPawn.WeaponServices.ActiveWeapon.Raw = weapon.Raw;
+                    // drop active weapon
+                    player.DropActiveWeapon();
                 }
-                // change weapon to currently active weapon
-                playerPawn.WeaponServices.ActiveWeapon.Raw = weapon.Raw;
-                // drop active weapon
-                player.DropActiveWeapon();
-                // delete it
-                weapon.Value.Remove();
             }
-            // put knife in hand of player
-            playerPawn.WeaponServices.ActiveWeapon.Raw = weaponRaw;
+            player.RemoveWeapons();
+            player.GiveNamedItem("weapon_knife");
             return Localizer["DiceStripWeapons"].Value
                 .Replace("{playerName}", player.PlayerName);
         }
