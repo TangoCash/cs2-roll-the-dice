3d1e6b9c34cc201a0fea08b5d10091af7d973fc2
Kalle Minkner <info@kalleminkner.de>
2024-11-11 12:15:36 +0100

    fix: automatic build only on Version.cs change

---
+++ src/Version.cs
@@ -0,0 +1,10 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace RollTheDice
+{
+    public partial class RollTheDice : BasePlugin
+    {
+        public override string ModuleVersion => "1.0.5";
+    }
+}
