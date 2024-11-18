699a3e6bff079a28bc8443d581d09eae6ea48254
Kalle <kalle@kandru.de>
2024-11-18 22:32:15 +0100

    fix: unused code

--- src/RollTheDice+DicePlayerLowGravity.cs
+++ src/RollTheDice+DicePlayerLowGravity.cs
@@ -4,8 +4,6 @@ namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
-        private List<CCSPlayerController> _playersWithLowGravity = new();
-
         private string DicePlayerLowGravity(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
             playerPawn.GravityScale = 0.4f;
