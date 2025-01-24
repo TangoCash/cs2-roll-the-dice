1b4dedee02c45aff91461b736b0c45c982e1e4c2
Kalle <kalle@kandru.de>
2025-01-24 20:56:04 +0100

    fix: optimize increasespeed

--- src/RollTheDice+DiceIncreaseSpeed.cs
+++ src/RollTheDice+DiceIncreaseSpeed.cs
@@ -5,8 +5,7 @@ namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
-        private List<CCSPlayerController> _playersWithIncreasedSpeed = new();
-        private Dictionary<CCSPlayerController, float> _playersWithIncreasedSpeedValue = new();
+        private Dictionary<CCSPlayerController, float> _playersWithIncreasedSpeed = new();
 
         private Dictionary<string, string> DiceIncreaseSpeed(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
@@ -16,10 +15,9 @@ namespace RollTheDice
             {
                 RegisterEventHandler<EventPlayerHurt>(EventDiceIncreaseSpeedOnPlayerHurt);
             }
-            _playersWithIncreasedSpeed.Add(player);
             var speedIncrease = _random.NextDouble() * ((float)config["max_speed"] - (float)config["min_speed"]) + (float)config["min_speed"];
-            playerPawn.VelocityModifier *= (float)speedIncrease;
-            _playersWithIncreasedSpeedValue[player] = (float)playerPawn.VelocityModifier;
+            _playersWithIncreasedSpeed.Add(player, (float)speedIncrease);
+            playerPawn.VelocityModifier *= (float)speedIncrease; ;
             Utilities.SetStateChanged(playerPawn, "CCSPlayerPawn", "m_flVelocityModifier");
             var percentageIncrease = (speedIncrease - 1.0) * 100;
             return new Dictionary<string, string>
@@ -38,25 +36,24 @@ namespace RollTheDice
         {
             DeregisterEventHandler<EventPlayerHurt>(EventDiceIncreaseSpeedOnPlayerHurt);
             // iterate through all players
-            foreach (var player in _playersWithIncreasedSpeed)
+            foreach (var kvp in _playersWithIncreasedSpeed)
             {
-                if (player == null || player.PlayerPawn == null || !player.PlayerPawn.IsValid || player.PlayerPawn.Value == null || player.LifeState != (byte)LifeState_t.LIFE_ALIVE) continue;
+                if (kvp.Key == null || kvp.Key.PlayerPawn == null || !kvp.Key.PlayerPawn.IsValid || kvp.Key.PlayerPawn.Value == null || kvp.Key.LifeState != (byte)LifeState_t.LIFE_ALIVE) continue;
                 // get player pawn
-                var playerPawn = player.PlayerPawn.Value!;
+                var playerPawn = kvp.Key.PlayerPawn.Value!;
                 // reset player speed
                 playerPawn.VelocityModifier = 1.0f;
                 // set state changed
                 Utilities.SetStateChanged(playerPawn, "CCSPlayerPawn", "m_flVelocityModifier");
             }
             _playersWithIncreasedSpeed.Clear();
-            _playersWithIncreasedSpeedValue.Clear();
         }
 
         private HookResult EventDiceIncreaseSpeedOnPlayerHurt(EventPlayerHurt @event, GameEventInfo info)
         {
             CCSPlayerController? victim = @event.Userid;
             if (victim == null) return HookResult.Continue;
-            if (!_playersWithIncreasedSpeed.Contains(victim)) return HookResult.Continue;
+            if (!_playersWithIncreasedSpeed.ContainsKey(victim)) return HookResult.Continue;
             if (victim == null || victim.PlayerPawn == null || !victim.PlayerPawn.IsValid || victim.PlayerPawn.Value == null || victim.LifeState != (byte)LifeState_t.LIFE_ALIVE) return HookResult.Continue;
             Server.NextFrame(() =>
             {
@@ -64,10 +61,10 @@ namespace RollTheDice
                     || !victim.IsValid
                     || victim.PlayerPawn == null
                     || !victim.PlayerPawn.IsValid
-                    || !_playersWithIncreasedSpeed.Contains(victim)) return;
+                    || !_playersWithIncreasedSpeed.ContainsKey(victim)) return;
                 CCSPlayerPawn playerPawn = victim.PlayerPawn.Value!;
                 // set player speed
-                playerPawn.VelocityModifier = _playersWithIncreasedSpeedValue[victim];
+                playerPawn.VelocityModifier *= _playersWithIncreasedSpeed[victim];
                 // set state changed
                 Utilities.SetStateChanged(playerPawn, "CCSPlayerPawn", "m_flVelocityModifier");
             });
