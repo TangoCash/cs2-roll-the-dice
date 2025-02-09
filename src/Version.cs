72fe74c0b8be82c4c2f01ede31cc5b1983bdd0d1
Kalle <kalle@kandru.de>
2025-02-09 23:17:38 +0100

    bump version to 1.2.8

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
-        public override string ModuleVersion => "1.2.7";
+        public override string ModuleVersion => "1.2.8";
     }
 }
